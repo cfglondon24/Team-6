ea44f0517f36720c7a9eb532126118589ec1ea40
Solomon Malih <smalih15@hotmail.com>
2024-04-11 12:04:55 +0100

    building flask api - added and called endpoint for symptoms

---
+++ app.py
@@ -0,0 +1,31 @@
+import json
+from flask import Flask, jsonify, request
+
+
+app = Flask(__name__)
+
+symptoms = []
+users = []
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register_user():
+    user_details = json.loads(request.data)
+    print(user_details)
+    users.append(user_details)
+    return '', 201
+
+
+
+@app.route('/symptoms', methods=['GET', 'POST'])
+def get_symptoms():
+    if request.method == 'POST':
+        symptom = json.loads(request.data)
+        symptoms.append(symptom)
+        print(symptoms)
+        return '', 201
+    return jsonify(symptoms)
+
+
+if __name__ == '__main__':
+    app.run(port=4000, debug=True)
